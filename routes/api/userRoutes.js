d3f3103a43d868a971bd6f8e99b72813dc729141
SamuelFullerCA <samuelfullerca@gmail.com>
2024-04-15 18:25:42 -0700

    node packages

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,4 +1,14 @@
-// GET ALL USERS
+const {User} = require('../../models');
+
+app.get('/', async (req, res) => {
+    try {
+      const result = await Department.find({});
+      res.status(200).json(result);
+    } catch (err) {
+      console.log('Uh Oh, something went wrong');
+      res.status(500).json({ error: 'Something went wrong' });
+    }
+  });
 
 
 // GET SING USER BY _id
